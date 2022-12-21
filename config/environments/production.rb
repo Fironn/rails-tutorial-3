9f015e7aadd1e7a65768796dc1ea6113dd95aee1
Ubuntu <shihomihashi@gmail.com>
2022-12-21 16:55:16 +0000

    update link render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sample-app-est7.onrender.com'
+  host = 'rails-tutorial-3.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
